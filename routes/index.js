3d9617fdcfc214bbdcaa3c328fcdb73ca26ad284
Ada Truong <adat456@gmail.com>
2023-07-12 10:44:29 -0400

    Include event tags in event and garden bed tables, modify existing endpoints and add means for deleting tags

--- routes/index.js
+++ routes/index.js
@@ -107,8 +107,8 @@ router.post("/create-bed", authenticate, async function(req, res, next) {
   try {
     // create the new bed and retrive its id
     const addNewBedReq = await pool.query(
-      "INSERT INTO garden_beds (hardiness, sunlight, soil, length, width, gridMap, name, public, created, username, numhearts, numcopies, seedbasket, members, roles) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *",
-      [hardiness, sunlight, soil, length, width, gridmapJSON, name, public, created, res.locals.username, 0, 0, JSON.stringify([]), JSON.stringify([]), JSON.stringify([])]
+      "INSERT INTO garden_beds (hardiness, sunlight, soil, length, width, gridMap, name, public, created, username, numhearts, numcopies, seedbasket, members, roles, eventtags) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) RETURNING *",
+      [hardiness, sunlight, soil, length, width, gridmapJSON, name, public, created, res.locals.username, 0, 0, JSON.stringify([]), JSON.stringify([]), JSON.stringify([]), []]
     );
     const newBoard = addNewBedReq.rows[0];
     const newBoardId = newBoard.id;
@@ -493,14 +493,29 @@ router.get("/pull-events/:bedid", authenticate, async function(req, res, next) {
 router.post("/add-event/:bedid", authenticate, async function(req, res, next) {
   let { bedid } = req.params;
   bedid = Number(bedid);
-  let { creatorId, creatorUsername, creatorName, eventName, eventDesc, eventLocation, eventPublic, eventParticipants, eventDate, eventStartTime, eventEndTime, repeating, repeatEvery, repeatTill, repeatId } = req.body;
+  let { creatorId, creatorUsername, creatorName, eventName, eventDesc, eventLocation, eventPublic, eventParticipants, eventDate, eventStartTime, eventEndTime, repeating, repeatEvery, repeatTill, repeatId, tags } = req.body;
   eventParticipants = JSON.stringify(eventParticipants);
 
   try {
     const req = await pool.query(
-      "INSERT INTO events (bedid, creatorid, creatorname, creatorusername, eventname, eventdesc, eventlocation, eventpublic, eventparticipants, eventstarttime, eventendtime, eventdate, repeating, repeatevery, repeattill, repeatid) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)",
-      [bedid, creatorId, creatorName, creatorUsername, eventName, eventDesc, eventLocation, eventPublic, eventParticipants, eventStartTime, eventEndTime, eventDate, repeating, repeatEvery, repeatTill, repeatId]
+      "INSERT INTO events (bedid, creatorid, creatorname, creatorusername, eventname, eventdesc, eventlocation, eventpublic, eventparticipants, eventstarttime, eventendtime, eventdate, repeating, repeatevery, repeattill, repeatid, tags) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)",
+      [bedid, creatorId, creatorName, creatorUsername, eventName, eventDesc, eventLocation, eventPublic, eventParticipants, eventStartTime, eventEndTime, eventDate, repeating, repeatEvery, repeatTill, repeatId, tags]
     );
+
+    const pullBedTagsReq = await pool.query(
+      "SELECT eventtags FROM garden_beds WHERE id = ($1)",
+      [bedid]
+    );
+    const currentEventTags = pullBedTagsReq.rows[0].eventtags;
+    let newEventTags = [];
+    tags.forEach(tag => {
+      if (!currentEventTags.includes(tag)) newEventTags.push(tag);
+    });
+    const updateBedTagsReq = await pool.query(
+      "UPDATE garden_beds SET eventtags = ($1) WHERE id = ($2)",
+      [[...currentEventTags, ...newEventTags], bedid]
+    );
+
     res.status(200).json("Event successfully added.");
   } catch(err) {
     console.log(err.message);
@@ -527,6 +542,45 @@ router.delete("/delete-event/:eventid/:repeatid", authenticate, async function(r
       );
       res.status(200).json("Event successfully deleted.");
     };
+  } catch(err) {
+    console.log(err.message);
+    res.status(404).json(err.message);
+  };
+});
+
+router.patch("/delete-tag/:bedid", authenticate, async function(req, res, next) {
+  let { bedid }  = req.params;
+  bedid = Number(bedid);
+  let { tag } = req.body;
+  tag = tag.trim().toLowerCase();
+
+  try {
+    const pullBedTagsReq = await pool.query(
+      "SELECT eventtags FROM garden_beds WHERE id = ($1)",
+      [bedid]
+    );
+    const updatedEventTags = pullBedTagsReq.rows[0].eventtags.filter(currentTag => currentTag !== tag);
+    const updateBedTagsReq = await pool.query(
+      "UPDATE garden_beds SET eventtags = ($1) WHERE id = ($2)",
+      [updatedEventTags, bedid]
+    );
+
+    const pullBedEventsReq = await pool.query(
+      "SELECT tags, id FROM events WHERE bedid = ($1)",
+      [bedid]
+    );
+    let tagsAndEventIds = pullBedEventsReq.rows;
+    tagsAndEventIds.forEach(async event => {
+      if (event.tags.includes(tag)) {
+        const updatedEventTags = event.tags.filter(currentTag => currentTag !== tag);
+        const updateEventReq = await pool.query(
+          "UPDATE events SET tags = ($1) WHERE id = ($2)",
+          [updatedEventTags, event.id]
+        );
+      };
+    });
+    res.status(200).json("Successfully deleted tag.");
+
   } catch(err) {
     console.log(err.message);
     res.status(404).json(err.message);
